ff5f4da5e031655df471888a123ed34ca89f1822
SuleymanEfe <cingozilecamgoz@gmail.com>
2020-05-02 02:46:54 +0300

    calisan v0.2 saat, dakika ve saniyeyi girerken 0 in altinda olunca hata veriyor tekrar girmesini istiyor aynisi 60/24 icinde gecerli. Eger saat 0 ise onu print etmiyor ornegin kalan zaman 1 saat 2 dakika 10 saniye olsun. 1 saat 2 dakikda 10 saniye kaldi.. seklinde print ediyor veya 2 dakika 20 saniye olsun. simdide 2 dakika 20 saniye kaldi seklinde print ediyor. (saniye icinde boyle) Artik daha farkli ve guzel bir yontemle zaman azaliyor.

--- sayac.py
+++ sayac.py
@@ -1,28 +1,70 @@
 import time
 import sys
 
-saat = int(input("Saat: "))
-dakika = int(input("Dakika: "))
-saniye = int(input("Saniye: "))
+while True:
+    saat = int(input("Saat: "))
+    if saat < 0:
+        print("Saat en az 0 olabilir. Lutfen tekrar girin.")
+        continue
+    elif saat > 24:
+        print("Saat en fazla 24 olabilir. Lutfen tekrar girin.")
+        continue
+    else:
+        break
 
-realsec = saniye
-realmin = dakika
-rearhour = saat
+while True:
+    dakika = int(input("Dakika: "))
+    if dakika < 0:
+        print("Dakika en az 0 olabilir. Lutfen tekrar girin.")
+        continue
+    elif dakika > 60:
+        print("Dakika en fazla 60 olabilir. Lutfen tekrar girin. ")
+        continue
+    else:
+        break
 
-if saat > 0:
-    dakika = dakika + saat * 60
-if dakika > 0:
-    saniye = saniye + dakika * 60
+while True:
+    saniye = int(input("Saniye: "))
+    if saniye < 0:
+        print("Saniye en az 0 olabilir. Lutfen tekrar girin.")
+        continue
+    elif saniye > 60:
+        print("Saniye en fazla 60 olabilir. Lutfen tekrar girin.")
+        continue
+    else:
+        break
 
-gecensure = 0
+
+saatkayit = saat
+dakikakayit = dakika
+saniyekayit = saniye
+
+
+kontrol = 0
+
 while True:
     time.sleep(1)
     saniye -= 1
     if saniye == 0:
-        print("ALARMM ALARMMMM ",saat, " saat ", dakika, "dakika", realsec, "saniye doldu" )
+        if dakika != 0:
+            dakika -= 1
+            saniye = 59
+        elif saat != 0:
+            saat -= 1
+            dakika = 59
+            saniye = 59
+        else:
+            kontrol = 1
+
+    if kontrol == 1:
+        print("Sayac bitti... ", saatkayit, " saat ", dakikakayit, "dakika", saatkayit, "saniye doldu" )
         break
     else:
-        print(saniye, "saniye gecti..")
-        print("TEST")
+        if saat != 0:
+            print(saat, "saat ", dakika, "dakika", saniye, "saniye kaldi..")
+        elif dakika != 0:
+            print(dakika, "dakika", saniye, "saniye kaldi..")
+        else:
+            print(saniye, "saniye kaldi..")
         continue
 
