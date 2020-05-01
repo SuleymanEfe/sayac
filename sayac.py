c4bd37b04303c22bd3d825487b6d10c52d658bcb
Suleyman Efe Celik <Suleymanefecelik@suleymans-mbp.local>
2020-05-02 00:57:32 +0300

    calisan v0.1

--- sayac.py
+++ sayac.py
@@ -1,16 +1,27 @@
 import time
 import sys
 
-
 saat = int(input("Saat: "))
-
-
 dakika = int(input("Dakika: "))
 saniye = int(input("Saniye: "))
 
+realsec = saniye
+realmin = dakika
+rearhour = saat
 
+if saat > 0:
+    dakika = dakika + saat * 60
+if dakika > 0:
+    saniye = saniye + dakika * 60
 
-
-
-
+gecensure = 0
+while True:
+    time.sleep(1)
+    saniye -= 1
+    if saniye == 0:
+        print("ALARMM ALARMMMM ",saat, " saat ", dakika, "dakika", realsec, "saniye doldu" )
+        break
+    else:
+        print(saniye, "saniye gecti..")
+        continue
 
